f27da4281581644e99c55f87e436e48d46717226
Sergey Skripnick <sskripnick@mirantis.com>
2015-06-15 22:42:40 +0300

    benchmark: fix keys generator script

--- benchmark/gen-keys.sh
+++ benchmark/gen-keys.sh
@@ -1,29 +1,26 @@
 
 mkdir keys
 
-for i in $(seq 1 4096); do
-
-echo $i;
-cat > keys/foo$i <<EOF
-     %echo Generating a standard key
-     Key-Type: DSA
-     Key-Length: 1024
-     Subkey-Type: ELG-E
-     Subkey-Length: 1024
-     Name-Real: Joe$i Tester
-     Name-Comment: with stupid passphrase
-     Name-Email: joe$i@foo.bar
-     Expire-Date: 0
-     Passphrase: abc$1
-     %pubring foo.pub
-     %secring foo.sec
-     # Do a commit here, so that we can later print "done" :-)
-     %commit
-     %echo done
+for i in $(seq 1 10000); do
+    cat > keys/foo$i <<EOF
+    %echo Generating a standard key
+    Key-Type: DSA
+    Key-Length: 1024
+    Subkey-Type: ELG-E
+    Subkey-Length: 1024
+    Name-Real: Joe$i Tester
+    Name-Comment: with stupid passphrase
+    Name-Email: joe$i@foo.bar
+    Expire-Date: 1y
+    Passphrase: abc$i
+    %pubring keys/foo$i.pub
+    %secring keys/foo$i.sec
+    %commit
+    %echo done
 EOF
-gpg --batch --gen-key keys/foo$i
-gpg --no-default-keyring --secret-keyring ./keys/foo$i.sec \
-    --keyring ./keys/foo$i.pub --list-secret-keys
+    gpg --batch --gen-key keys/foo$i
+    gpg --no-default-keyring --secret-keyring ./keys/foo$i.sec \
+        --keyring ./keys/foo$i.pub --list-secret-keys
 
 done
 
